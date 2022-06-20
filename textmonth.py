8c633660fceb93e10bfea4f040e7be6f2c36e845
VladislavZarubin <106274858+VladislavZarubin@users.noreply.github.com>
2022-06-20 20:11:28 +0300

    Create textmonth.py

---
+++ textmonth.py
@@ -0,0 +1,15 @@
+# объявление функции
+def get_month(language, number):
+    ru =['январь', 'февраль', 'март', 'апрель','май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
+    en =['january', 'february', 'march', 'april','may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+    if language == 'en':
+        return en[number-1]
+    if language == 'ru':
+        return ru[number-1]
+
+# считываем данные
+lan = input()
+num = int(input())
+
+# вызываем функцию
+print(get_month(lan, num))
